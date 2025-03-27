78cf5daba2631899801260396718d8a5fc8b0e67
hjzhhhj <jhj090120@gmail.com>
2025-03-27 12:11:07 +0900

    ㅎ

---
+++ recursion.mjs
@@ -0,0 +1,13 @@
+function myFunction(number) {
+    if ( number > 3 ) return;
+    console.log(number)
+    myFunction(number + 1)
+}
+
+myFunction(1);
+
+// for문을 사용해서 1~10까지 출력
+
+for ( let i = 1; i <= 10; i++ ) {
+    console.log(i);
+}
