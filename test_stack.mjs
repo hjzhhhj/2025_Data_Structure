5e50862e9cf39124f484871b7ae9c8d2d316733e
hjzhhhj <jhj090120@gmail.com>
2025-04-28 11:36:39 +0900

    배열로 스택 구현

---
+++ test_stack.mjs
@@ -0,0 +1,17 @@
+import { LinkedList } from "./LinkedList.mjs";
+
+let stack = [];
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+
+console.log(stack);
+
+console.log(stack.pop())
+console.log(stack.pop())
+console.log(stack.pop())
+
+console.log(stack)
